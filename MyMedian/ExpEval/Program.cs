eb139e27220eb364a81e69ad8f21431a448c557b
nobel@huskers.unl.edu <nobel@huskers.unl.edu>
2017-08-19 06:58:54 -0700

    Desktop project

---
+++ MyMedian/ExpEval/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace ExpEval
+{
+
+    public class ExpEval
+    {
+        private Stack<string> store = new Stack<string>();
+
+
+        public int Evaluate(string input)
+        {
+            if(string.IsNullOrEmpty(input))
+            {
+                return 0;
+            }
+
+            // parse to string
+            int evaluation = 0;
+
+            // store          
+            int index=0;
+            while(index<input.Length)
+            {
+                // push case
+                //number, +, -
+                if(input[index].Equals("+") 
+                || 
+                input[index].Equals("-")
+                ||
+                input[index].Equals("(")
+                )
+                {
+                    this.store.Push(input[index].ToString());
+                }
+
+                else if(Char.IsNumber(((char) input[index])))
+                {
+                    StringBuilder tmp = new StringBuilder();
+                    
+                    while(Char.IsNumber(input[index]))
+                    {
+                        tmp.Append(input[index].ToString());
+                        index++;
+                    }
+                    
+                    this.store.Push(tmp.ToString());
+                    continue;
+                }
+
+                // pop/eval case                
+                else if (input[index].Equals("=")
+                ||
+                input[index].Equals(")"))
+                {
+                    string current = this.store.Pop();
+                    while(this.store.Count>0)
+                    {
+                        if(current=="+")
+                        {
+                            evaluation+=Int32.Parse(this.store.Pop().ToString());
+                        }
+                        else if(current=="-")
+                        {
+                            evaluation+=Int32.Parse(this.store.Pop().ToString());
+                        }
+                        else if(current=="(")
+                        {
+                            this.store.Push(evaluation.ToString());
+                        }
+                    }
+                }
+
+
+            index++;
+
+            }
+
+
+            // return answer
+            return evaluation;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MyMedian/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace MyMedian
+{
+
+    public class MyMedian
+    {
+
+        private int[] data = new int[10];
+
+        private int currentSize = 0;
+
+        public MyMedian()
+        {
+
+        }
+
+        public void Add(int value)
+        {
+            // boundary condition
+            if(currentSize==this.data.Length-1)
+            {
+                Array.Resize(ref this.data, this.data.Length*2);
+            }
+
+
+            // calculate position
+            int insertIndex = 0;
+
+            while(insertIndex<this.data.Length)
+            {
+                if(value<this.data[insertIndex] || insertIndex>=this.currentSize)
+                {
+                    break;
+                }
+
+                insertIndex++;
+            }
+
+            for(int index=currentSize; index>insertIndex; index--)
+            {
+                this.data[index] = this.data[index-1];
+            }
+            
+            // insert data
+            // increment size
+            this.data[insertIndex]=value;
+            this.currentSize++;
+       }
+
+       public double GetMedian()
+       {
+           if(this.currentSize==0)
+           {
+               return 0;
+           }
+           else if(this.currentSize==1)
+           {
+               return this.data[0];
+           }
+           else if(this.currentSize%2==0)
+           {
+                return (double)(this.data[this.currentSize/2]+this.data[(this.currentSize/2)-1])/2.0d;
+           }
+           else
+           {
+               return (double)this.data[this.currentSize/2];
+           }
+       }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyMedian m = new MyMedian();
+            m.Add(2);
+            m.Add(100);
+            m.Add(1000);
+            Console.WriteLine(m.GetMedian());
+        }
+    }
+}
