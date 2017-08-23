5d23ecf8a2a8951956b543a97dea59000b415ce4
Nobel Khandaker <nobel@huskers.unl.edu>
2017-08-22 19:00:34 -0700

    Moving Average complete

--- MovingAverage/Program.cs
+++ MovingAverage/Program.cs
@@ -1,7 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MovingAverage
 {
+    public class MovingAverage
+    {
+
+        private int maxDataSize = 0;
+        private int currentDataSize = 0;
+        private int currentSum = 0;
+        private Queue<int> dataQueue = new Queue<int>();
+
+        /** Initialize your data structure here. */
+        public MovingAverage(int size)
+        {
+
+            this.maxDataSize = size;
+        }
+
+        public double Next(int val)
+        {
+            if (currentDataSize < maxDataSize)
+            {
+                currentDataSize++;
+                currentSum += val;
+                dataQueue.Enqueue(val);
+
+                return currentSum / (double)currentDataSize;
+            }
+
+            else
+            {
+                dataQueue.Enqueue(val);
+                currentSum = currentSum - dataQueue.Dequeue() + val;
+                return currentSum / (double)maxDataSize;
+            }
+        }
+    }
+
+    /**
+     * Your MovingAverage object will be instantiated and called as such:
+     * MovingAverage obj = new MovingAverage(size);
+     * double param_1 = obj.Next(val);
+     */
+
     class Program
     {
         static void Main(string[] args)
