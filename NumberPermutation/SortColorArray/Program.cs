0f2cf03b67c21b7d8b70c37b9816b395a48e1a49
Nobel Khandaker <nobel@huskers.unl.edu>
2017-09-06 03:26:48 -0700

    Sorted color array

--- NumberPermutation/SortColorArray/Program.cs
+++ NumberPermutation/SortColorArray/Program.cs
@@ -2,6 +2,50 @@
 
 namespace SortColorArray
 {
+    public class Solution
+    {
+
+        public void SortColors(int[] nums)
+        {
+
+            if (nums == null || nums.Length == 0)
+            {
+                return;
+            }
+
+            int lowIndex = 0;
+            int highIndex = nums.Length - 1;
+            int temp = 0;
+
+            for (int index = lowIndex; index <= highIndex;)
+            {
+
+                if (nums[index] < 1)
+                {
+                    temp = nums[lowIndex];
+                    nums[lowIndex] = nums[index];
+                    nums[index] = temp;
+                    lowIndex++;
+                    index++;
+                }
+                else if (nums[index] > 1)
+                {
+
+                    temp = nums[highIndex];
+                    nums[highIndex] = nums[index];
+                    nums[index] = temp;
+                    highIndex--;
+                }
+                else
+                {
+                    index++;
+                }
+            }
+
+            return;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
