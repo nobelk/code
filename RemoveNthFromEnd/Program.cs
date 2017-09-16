4c750c3aedfb7e30901bf99cc465c5c2bf1da751
Nobel Khandaker <nobel@huskers.unl.edu>
2017-09-15 17:50:21 -0700

    RemoveNthFromEnd

---
+++ RemoveNthFromEnd/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace RemoveNthFromEnd
+{
+    public class ListNode
+    {
+        public int value;
+        public ListNode next;
+
+        public ListNode(int value)
+        {
+            this.value = value;
+            next = null;
+        }
+    }
+
+    class RemoveNthFromEnd
+    {
+
+
+        public ListNode RemoveNthFromEnd(ListNode head, int n) 
+        {
+
+            if(head == null || head.next == null)
+            {
+                return null;
+            }
+
+            ListNode start = new ListNode(0);
+            start.next = head;
+            ListNode slowRunner = start;
+            ListNode fastRunner = start;
+
+            int counter=1;
+            while(fastRunner.next != null)
+            {
+                fastRunner = fastRunner.next;
+
+                if(counter < n)
+                {
+                    counter++;
+                }
+                else
+                {
+                    slowRunner = slowRunner.next;
+                }
+            }
+
+            slowRunner.next = slowRunner.next.next;
+
+            return start.next;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
