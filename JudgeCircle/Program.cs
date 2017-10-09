37fb3c504e9859ee64a4a0436604b22552e58626
nobel@huskers.unl.edu <nobel@huskers.unl.edu>
2017-10-09 04:11:44 -0700

    SOlution

---
+++ JudgeCircle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace JudgeCircle
+{
+	class Program
+	{
+
+		public static bool JudgeCircle(string moves)
+		{
+
+			if(string.IsNullOrEmpty(moves))
+				return true;
+
+			Dictionary<char,int> directionCount = new Dictionary<char,int>();
+
+			directionCount.Add('L',0);
+			directionCount.Add('R',0);
+			directionCount.Add('U',0);
+			directionCount.Add('D',0);
+
+			for(int index=0; index<moves.Length; index++)
+			{
+				directionCount[moves[index]] = directionCount[moves[index]] + 1;
+			}
+
+			return      directionCount['U'] == directionCount['D'] 
+				&&
+				directionCount['L'] == directionCount['R'];
+
+		}
+		static void Main(string[] args)
+		{
+			Console.WriteLine(JudgeCircle("UD"));	
+		}
+	}
+}
