7fd741a48e12cf28275fba664fd716f7e5ed598f
nobel khandaker <nobel@huskers.unl.edu>
2021-06-17 07:30:11 +0600

    Reorganized problem-solutions file

--- java/problem-solution/src/main/java/io/isoftware/app/ListNode.java
+++ java/problem-solution/src/main/java/io/isoftware/app/ListNode.java
@@ -3,7 +3,8 @@ package io.isoftware.app;
 public class ListNode {
     int val;
     ListNode next;
-    ListNode(int x){
-        val = x;
+    ListNode(int val){
+        this.val = val;
     }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
